5b446c9f4fb02873f94c83af07e8ab112de8e30d
Muskan <muskangulia72@gmail.com>
2026-06-27 17:33:12 +0530

    Use environment database URL for Alembic migrations

--- alembic/env.py
+++ alembic/env.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from settings import settings
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from logging.config import fileConfig
@@ -12,6 +13,10 @@ from alembic import context
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+config.set_main_option(
+    "sqlalchemy.url",
+    settings.DATABASE_URL
+)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
