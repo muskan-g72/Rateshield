cfb957674dcf028d6ce840a8856e4f4a0cabe95f
Muskan <muskangulia72@gmail.com>
2026-06-28 15:28:51 +0530

    Fix Vercel SPA routing

--- main.py
+++ main.py
@@ -51,6 +51,7 @@ app.add_middleware(
         "http://localhost:5500",
         "http://127.0.0.1:5173",
         "http://localhost:5173",
+        "https://rateshield-nine.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
